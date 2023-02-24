b22ca953b20a9f629a66f060ff4e7c757313330e
hanishwoona <woonahanish@gmail.com>
2023-02-24 01:15:44 -0600

    added FWChaining.cpp

---
+++ Project 1/FWChaining.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<map>
+#include<string>
+#include<vector>
+#include<queue>
+#include <iterator>
+#include<fstream>
+
+using namespace std;
+
+class FWChaining {
+public:
+    map<int,string> clauseVariableList;
+    map<int,string> knowledgeBase;
+    queue<string> conclusionVariableQueue;
+    map<string,bool> variableList;
+    map<int,int> clauseVariablePointer;
+
+    FWChaining() {
+
+        knowledgeBase[10] = "IF BasicAttack=true THEN Prevention 1";
+        knowledgeBase[20] = "IF IdentityAttack=true THEN Prevention 2";
+        knowledgeBase[30] = "IF DenialOfService=true THEN Prevention 3";
+        knowledgeBase[40] = "IF MaliciousCode=true THEN Prevention 4";
+
+        clauseVariableList[1] = "BasicAttack";
+        clauseVariableList[2] = "";
+        clauseVariableList[3] = "";
+        clauseVariableList[4] = "";
+        clauseVariableList[5] = "IdentityAttack";
+        clauseVariableList[6] = "";
+        clauseVariableList[7] = "";
+        clauseVariableList[8] = "";
+        clauseVariableList[9] = "DenialOfService";
+        clauseVariableList[10] = "";
+        clauseVariableList[11] = "";
+        clauseVariableList[12] = "";
+        clauseVariableList[13] = "MaliciousCode";
+        clauseVariableList[14] = "";
+        clauseVariableList[15] = "";
+        clauseVariableList[16] = "";
+
+    }
+
+    int search_cvl(string clause) {
+        int ci;
+        for(int i=1; i<=16; i++){
+            if(clauseVariableList[i]==clause) {
+                ci = i;
+                break;
+            }
+            else ci = 0;
+        }
+        return ci;
+    }
+
+    int clause_to_rule(int ci) {
+        int ri = ((ci/4)+1)*10;
+        return ri;
+    }
+
+    void update_VL(int ci) {
+        variableList[clauseVariableList[ci]] = true;
+    }
+
+    void validate_Ri(int ri) {
+        for(int i=1; i<=16; i+4){
+            string clause = clauseVariableList[i];
+            if(variableList[clause]==true && knowledgeBase[ri].find(clause)!=0) {
+                conclusionVariableQueue.push(clause);
+                break;
+            }
+        }
+    }
+
+    void process(string clause) {
+
+        variableList["BasicAttack"] = false;
+        variableList["IdentityAttack"] = false;
+        variableList["DenialOfService"] = false;
+        variableList["MaliciousCode"] = false;
+
+        search_cvl(clause);
+
+    }
+
+};
